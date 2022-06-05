061781fddcaddb0c757fc51803b08f61919012bb
Collins Mundia <collins.mwangi@student.moringaschool.com>
2022-06-05 20:18:10 +0300

    Add html css and js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+alert(document.head.firstElementChild.innerHTML="First Project");
+if (confirm("Do you love this web?")) {
+    alert("You pressed OK. Thank you.");
+    
+} else {
+    alert("You pressed cancel");
+    
+}
