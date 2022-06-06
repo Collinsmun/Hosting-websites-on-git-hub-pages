29c131b48b41d7d6f69cf74b50df8761effd2319
Collins Mundia <collins.mwangi@student.moringaschool.com>
2022-06-06 22:09:32 +0300

    Remade project

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
-alert(document.head.firstElementChild.innerHTML="First Project");
-if (confirm("Do you love this web?")) {
-    alert("You pressed OK. Thank you.");
-    
-} else {
-    alert("You pressed cancel");
-    
-}
+$(document).ready(function(){
+    $(window).scroll(function() {
+        if (this.scrollY > 20) {
+    $('.navbar').addClass("sticky");
+        } else {
+    $('.navbar').removeClass("sticky");
+            
+        }
+    })
+});
